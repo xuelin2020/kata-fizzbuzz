b3d94fb2a402000ec90067b5062df5893bebe6fb
Selvin Xue <xuexuedada@icloud.com>
2022-01-24 16:15:45 +0800

    Create expected obj.

---
+++ src/test/java/com/example/project/TestGameNumberTest.java
@@ -0,0 +1,11 @@
+package com.example.project;
+
+import org.junit.jupiter.api.Test;
+
+public class TestGameNumberTest {
+
+    @Test
+    void should_create_game_number_from_raw_number() {
+        GameNumber gameNumber = new GameNumber();
+    }
+}
