eb8beb49a13b7376d89c2525206ee5ec873c4bb0
Selvin Xue <xuexuedada@icloud.com>
2022-01-24 16:35:23 +0800

    Add test,

---
+++ src/main/java/com/example/project/Game.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+public class Game {
+
+
+    private int size;
+
+    public Game(int size) {
+        this.size = size;
+    }
+
+    public int size() {
+        return size;
+    }
+}
--- src/test/java/com/example/project/GameNumberTest.java
+++ src/test/java/com/example/project/GameNumberTest.java
@@ -1,31 +1,40 @@
 package com.example.project;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TestGameNumberTest {
+public class GameNumberTest {
 
     @Test
     void should_say_1_when_raw_number_is_1(){
         GameNumber gameNumber = new GameNumber(1);
-        Assertions.assertEquals(gameNumber.toString(), ("1"));
+        assertEquals(gameNumber.toString(), ("1"));
     }
 
     @Test
     void should_say_3_when_raw_number_is_Fizz(){
         GameNumber gameNumber = new GameNumber(3);
-        Assertions.assertEquals(gameNumber.toString(), ("Fizz"));
+        assertEquals(gameNumber.toString(), ("Fizz"));
     }
 
     @Test
     void should_say_5_when_raw_number_is_Buzz(){
         GameNumber gameNumber = new GameNumber(5);
-        Assertions.assertEquals(gameNumber.toString(), ("Buzz"));
+        assertEquals(gameNumber.toString(), ("Buzz"));
     }
 
     @Test
     void should_say_15_when_raw_number_is_FizzBuzz(){
         GameNumber gameNumber = new GameNumber(15);
-        Assertions.assertEquals(gameNumber.toString(), ("FizzBuzz"));
+        assertEquals(gameNumber.toString(), ("FizzBuzz"));
+    }
+
+
+    @Test
+    void should_create_game_object_with_given_size(){
+        Game game = new Game(100);
+        assertEquals(game.size(), 100);
     }
 }
