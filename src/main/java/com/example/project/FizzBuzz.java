62a66fb5924e5f2a8f37ae27de30984ec1aa7d9a
Selvin Xue <xuexuedada@icloud.com>
2022-01-24 14:42:11 +0800

    Add test 3 multiple is ture, passed.

--- src/main/java/com/example/project/FizzBuzz.java
+++ src/main/java/com/example/project/FizzBuzz.java
@@ -2,4 +2,7 @@ package com.example.project;
 
 public class FizzBuzz {
 
+    public static boolean isThreeMultiple(int num) {
+        return false;
+    }
 }
--- src/test/java/com/example/project/FizzBuzzTest.java
+++ src/test/java/com/example/project/FizzBuzzTest.java
@@ -1,6 +1,25 @@
 package com.example.project;
 
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class FizzBuzzTest {
+
+    public static Stream<Integer> threeMultipleList() {
+        return Stream.of(3, 6, 9, 12);
+    }
+
+    @ParameterizedTest
+    @MethodSource("threeMultipleList")
+    void should_return_true_when_input_parameter_is_3_multiple(int num) {
+        assertTrue(FizzBuzz.isThreeMultiple(num));
+    }
+
 
 }
