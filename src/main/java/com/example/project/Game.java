3a841da7b9815b7022a6cb9f4d998fb6021a1600
Selvin Xue <xuexuedada@icloud.com>
2022-01-24 16:58:04 +0800

    fix

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -10,7 +10,7 @@ public class Game {
 
     public Game(int size) {
         gameNumbers = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
+        for (int i = 1; i <= size; i++) {
             gameNumbers.add(new GameNumber(i));
         }
     }
