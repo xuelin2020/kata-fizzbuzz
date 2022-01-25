4b55a677334ccd88a827f1bc92abeac278258017
Selvin Xue <xuexuedada@icloud.com>
2022-01-25 08:13:35 +0800

    Test, passed.

--- src/main/java/com/example/project/GameNumber.java
+++ src/main/java/com/example/project/GameNumber.java
@@ -15,7 +15,7 @@ public class GameNumber {
 
 
     public String coveredWord() {
-        if (number == 3){
+        if (number %3 == 0){
             return "Fizz";
         }
         if (number == 5){
