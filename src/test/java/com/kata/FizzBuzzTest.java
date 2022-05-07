a37d1d65e97499ef698bfb4c6193a4a8c634dd92
Selvin Xue <xuexuedada@icloud.com>
2022-05-08 01:16:13 +0800

    init project.

---
+++ src/test/java/com/kata/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.kata;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    void should_work_on() {
+        assertEquals(1, 1);
+    }
+}
